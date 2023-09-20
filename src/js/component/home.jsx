c5ead5b28ae85b3ac2180bfaeee44baa845cfc75
Favo4gabo <137852920+Favo4gabo@users.noreply.github.com>
2023-09-20 04:03:43 +0000

    ready?

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,17 @@
 import React from "react";
 
+
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex justify-content-center">
+			<h1 className="p-3 bg-dark text-white border border-secondary">{props.four}</h1>
+			<h1 className="p-3 bg-dark text-white border border-secondary">{props.three}</h1>
+			<h1 className="p-3 bg-dark text-white border border-secondary">{props.two}</h1>
+			<h1 className="p-3 bg-dark text-white border border-secondary">{props.one}</h1>						
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,19 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0
+
+setInterval (() => {
+
+    let one = Math.floor(counter / 1 % 10)
+    let two = Math.floor(counter / 10 % 10)
+    let three = Math.floor(counter / 100 % 10)
+    let four = Math.floor(counter / 1000 % 10)
+
+    counter++
+
+    //render your react application
+ReactDOM.render(<Home one={one} two={two} three={three} four={four} />, document.querySelector("#app"));
+
+    
+}, 1000)
